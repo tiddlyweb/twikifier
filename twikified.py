1934cf6ee64dd937aecf9ae7975d6745f236ed80
Chris Dent <chris.dent@gmail.com>
2011-03-14 16:41:18 +0000

    First spike at the Python side of a twikifier based serialization for tiddlers.
    All this does is assemble the necessary HTML so that the right javascript is
    in the right place at the right time.
    
    Issues and things to fix:
    
    * If a tiddler is not tiddlywiki text, but is in the wikitext
      type_render_map, we need to call super from this code, not
      use this code. (Note: true binaries will never reach this code)
    
    * Instead of retrieving the wikitext from the server as json, we
      should include it in the sent HTML. Otherwise we have all the same
      problems with SEO that TiddlyWiki itself has. The question is whether
      * to include the wikklytext rendered HTML, and then "improve" it
        on the fly
      * do the text wrapped in pre, as done in TiddlyWiki
      The latter would be faster, the former might be prettier.
    
    * can we unicode?

---
+++ twikified.py
@@ -0,0 +1,58 @@
+"""
+Serialization that uses twikifier to render.
+
+Assumes for the time being that the necessary code 
+is relative to the current page. Will come from config
+eventually?
+"""
+
+from tiddlyweb.serializations.html import Serialization as HTMLSerialization
+
+from tiddlyweb.web.util import escape_attribute_value
+
+SERIALIZERS = {
+    'text/html': ['twikified', 'text/html; charset=UTF-8'],
+    'default': ['twikified', 'text/html; charset=UTF-8'],
+}
+
+
+def init(config):
+    config['serializers'].update(SERIALIZERS)
+
+
+class Serialization(HTMLSerialization):
+
+    def tiddler_as(self, tiddler):
+        """
+        Send out the bare minimum required to make webtwik know there is 
+        a tiddler.
+        """
+        common_container = self.environ.get(
+                'tiddlyweb.config', {}).get(
+                        'twikified.container', '/bags/common/tiddlers/')
+        scripts = """
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"
+    type="text/javascript"></script>
+<script>
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-ControlView",
+            "false");
+        }
+    });
+</script>
+<script src="%(container)stwikifier" type="text/javascript"></script>
+<script src="%(container)stwik" type="text/javascript"></script>
+<script src="%(container)swebtwik" type="text/javascript"></script>
+""" % {'container': common_container}
+        tiddler = '<div class="tiddler" title="%s" %s></div>' % (
+                escape_attribute_value(tiddler.title),
+                self._tiddler_provenance(tiddler))
+        self.environ['tiddlyweb.title'] = tiddler.title
+        return tiddler + scripts
+
+    def _tiddler_provenance(self, tiddler):
+        if tiddler.recipe:
+            return 'server.recipe="%s"' % escape_attribute_value(tiddler.recipe)
+        else:
+            return 'server.bag="%s"' % escape_attribute_value(tiddler.bag)
