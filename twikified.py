42975410363e085ab7b8f58af422be03c71d6eda
Chris Dent <chris.dent@gmail.com>
2011-04-06 14:55:57 +0100

    Get command line working for server-side rendering.
    twikified.py has been extended to also be wikitext renderer. It uses
    the subprocess module to open a pipe to twikify. The current collection
    (bag or recipe tiddlers uri) is loaded into twikify, and the text of
    the curren tiddler is passed on STDIN. The results come on STDOT and
    are returned from the renderer to the serialization.
    
    tiddlywebconfig.py can be used to control whether server side or
    client rendering is done.
    
    Problems:
    
    * **Only content readable by GUEST can be rendered.** Short term may
      be able to tweak on the current user such that for HTML we render
      client side and for Atom we render with wikklytext when doing
      private content. Longer term there may be some ways to engage auth.
    * twikify does no caching, so when rendering many tiddlers from the
      same collection there can be a lot of reduncancy. The current model
      does not easily allow a long running twikify process, but one could
      be possible. Might better to have an on disk or in memcached cache
      and do ETag matching.
    * What should happen for a tiddler that is in search results.
      The only reason for passing a collection is to get macros that look
      at the store working (somewhat) so search results do not a reasonable
      store make. In the current code each tiddler gets its own collection
      based on its recipe or bag. In search results tiddlers only have bags.

--- twikified.py
+++ twikified.py
@@ -1,14 +1,30 @@
 """
 Serialization that uses twikifier to render.
 
-Assumes for the time being that the necessary code 
-is relative to the current page. Will come from config
-eventually?
+It can render in two ways. If config['twikified.render'] is
+True, then rendering will be done serverside against a command
+line node client called twikify. Otherwise rendering will be
+delegated to the client.
+
+The twikify script is located at config['twikified.twikify'],
+'./twikify' by default. NODE_PATH handling is up to the installer
+at this point.
+
+If client side rendering is used, then a bunch of javascript is
+expected to be found in config['twikified.container'], defaulting
+to '/bags/common/tiddlers/'. The installer is expected to get the
+right tiddlers in the right place (for now).
 """
 
-from tiddlyweb.serializations.html import Serialization as HTMLSerialization
+import subprocess
 
-from tiddlyweb.web.util import escape_attribute_value, html_encode
+from tiddlywebplugins.atom.htmllinks import Serialization as HTMLSerialization
+
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.recipe import Recipe
+from tiddlyweb.web.util import (escape_attribute_value, html_encode,
+        encode_name, recipe_url, bag_url)
+
 
 SERIALIZERS = {
     'text/html': ['twikified', 'text/html; charset=UTF-8'],
@@ -18,15 +34,37 @@ SERIALIZERS = {
 
 def init(config):
     config['serializers'].update(SERIALIZERS)
+    config['wikitext.default_renderer'] = 'twikified'
+
+
+def render(tiddler, environ):
+    if tiddler.recipe:
+        collection = recipe_url(environ, Recipe(tiddler.recipe)) + '/tiddlers'
+    else:
+        collection = bag_url(environ, Bag(tiddler.bag)) + '/tiddlers'
+    # let any errors raise themselves
+    twikify = environ['tiddlyweb.config'].get('twikified.twikify',
+            './twikify')
+    text = subprocess.Popen([twikify, '--collection=%s' % collection],
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE).communicate(tiddler.text)[0]
+    return text.decode('UTF-8')
 
 
 class Serialization(HTMLSerialization):
+
+    def _render(self, tiddler):
+        return HTMLSerialization.tiddler_as(self, tiddler)
 
     def tiddler_as(self, tiddler):
         """
         Send out the bare minimum required to make webtwik know there is 
         a tiddler.
         """
+        # branch away if we are going to use the render system
+        if self.environ['tiddlyweb.config'].get('twikified.render', True):
+            return self._render(tiddler)
+
         common_container = self.environ.get(
                 'tiddlyweb.config', {}).get(
                         'twikified.container', '/bags/common/tiddlers/')
