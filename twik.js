b2d24664e98bd89ddb7da9f4a6b67f7380685823
Chris Dent <chris.dent@gmail.com>
2013-02-28 12:27:47 +0000

    jslint cleanups
    Most important fix here is making sure storeURL is not a global.

--- twik.js
+++ twik.js
@@ -1,10 +1,13 @@
+/*jslint unparam: true, indent: 4, node: true */
+
+"use strict";
+
 // utility funcions for twikify and twikifier related things
-//
 var twik = {};
 
 twik.formatText = function(window, wikify, text) {
     // create a browser window
-    var place = window.document.getElementById('tiddler');;
+    var place = window.document.getElementById('tiddler');
 
     // wikify the tiddler text.
     // XXX This should be functional, returning a string,
@@ -12,17 +15,19 @@ twik.formatText = function(window, wikify, text) {
     wikify(text, place, null, null);
 
     return window.document.innerHTML;
-}
+};
 
 twik.loadRemoteTiddlers = function(store, Tiddler, uri, jsonTiddlers) {
-    var tiddlers = JSON.parse(jsonTiddlers);
-    for (var i = 0; i < tiddlers.length ; i++) {
+    var tiddlers = JSON.parse(jsonTiddlers),
+		i,
+		storeURL;
+    for (i = 0; i < tiddlers.length; i++) {
         var t = new Tiddler(tiddlers[i].title);
         //t.text = tiddlers[i].text;
         t.tags = tiddlers[i].tags;
         t.modifier = tiddlers[i].modifier;
-        t.modified = Date.convertFromYYYYMMDDHHMM(tiddlers[i].modified)
-        t.created = Date.convertFromYYYYMMDDHHMM(tiddlers[i].created)
+        t.modified = Date.convertFromYYYYMMDDHHMM(tiddlers[i].modified);
+        t.created = Date.convertFromYYYYMMDDHHMM(tiddlers[i].created);
         t.fields = tiddlers[i].fields;
         store.addTiddler(t);
     }
@@ -30,12 +35,10 @@ twik.loadRemoteTiddlers = function(store, Tiddler, uri, jsonTiddlers) {
     storeURL.text = uri;
     storeURL.tags = ['excludeLists'];
     store.addTiddler(storeURL);
-}
+};
 
 
-if (typeof exports != "undefined") {
+if (exports !== "undefined") {
     exports.formatText = twik.formatText;
     exports.loadRemoteTiddlers = twik.loadRemoteTiddlers;
 }
-
-
