d07c8d8efa2dfc1f013047985744a64cecb5deee
Ben Paddock <pads@thisispads.me.uk>
2013-03-20 16:58:28 +0000

    Corrected single quotes typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,5 +87,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-curl");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks('grunt-release');
+    grunt.loadNpmTasks("grunt-release");
 };
